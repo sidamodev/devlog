21bf940837de42a4438d89724a436e25fb066c23
sidamodev <sidamo.dev@gmail.com>
2026-01-12 18:51:13 +0900

    refactor: browser, server 환경에서 isomorphic fetch wrapper를 사용하도록 수정

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,8 +1,5 @@
+import { api } from '@/lib/http/api-client';
 import type { PostList } from '@/types/post';
-import { api as serverApi } from '../lib/http/api.server';
-import { api as browserApi } from '../lib/http/api.browser';
-
-const api = typeof window === 'undefined' ? serverApi : browserApi;
 
 export const getPostList = async () => {
   const response = await api.get<PostList>('/');
--- src/lib/http/api.browser.ts
+++
@@ -1,7 +0,0 @@
-import { ApiClientCore } from './api.core';
-
-const baseURL = process.env.NEXT_PUBLIC_API_URL!;
-
-export const api = new ApiClientCore(baseURL, {
-  'Content-Type': 'application/json',
-});
