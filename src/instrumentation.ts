72cdb152b6c93b7968c8e859c4a7b1d5e563faae
sidamodev <sidamo.dev@gmail.com>
2026-01-13 13:28:37 +0900

    fix: env 확인해서 MSW 등록하도록 수정

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,10 @@
 export async function register() {
   // 서버 환경에서 MSW 등록
-  if (process.env.NODE_ENV === 'development' && process.env.NEXT_RUNTIME === 'nodejs') {
+  if (
+    process.env.NODE_ENV === 'development' &&
+    process.env.NEXT_RUNTIME === 'nodejs' &&
+    process.env.NEXT_PUBLIC_API_MOCKING === 'true'
+  ) {
     const { server } = await import('./mocks/server');
     server.listen({
       onUnhandledRequest: 'bypass',
