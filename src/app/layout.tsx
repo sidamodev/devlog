3d6c2576cb6144a16b73b3f07762ec14af43d9a4
sidamodev <sidamo.dev@gmail.com>
2026-01-14 23:46:46 +0900

    design: 기본 폰트 Pretendard로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,21 @@ import Providers from '@/app/providers';
 import AppHeader from '@/components/header/app-header';
 import AppNav from '@/components/navigation/app-nav';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920', // WebKit 렌더링 버그 방지용 필수
+  variable: '--font-pretendard',
 });
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+  weight: ['100', '900'],
 });
 
 export const metadata: Metadata = {
@@ -27,7 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-accent/60 dark:bg-background`}>
+      <body
+        className={`${pretendard.variable} ${geistMono.variable} font-sans antialiased bg-accent/60 dark:bg-background`}
+      >
         <Providers>
           <div className="flex min-h-screen">
             <AppNav />
