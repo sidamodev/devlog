a4dc02b5458c61b29b42eb6f3e389783db110d73
sidamodev <sidamo.dev@gmail.com>
2026-01-21 13:33:03 +0900

    refactor: AppNav 버튼 인라인화 및 search, stats 페이지 추가

--- src/app/notification/page.tsx
+++
@@ -1,4 +0,0 @@
-const NotificationPage = () => {
-  return <div>NotificationPage</div>;
-};
-export default NotificationPage;
--- src/components/navigation/app-nav.tsx
+++ src/components/navigation/app-nav.tsx
@@ -1,31 +1,25 @@
 'use client';
-import NavButton from '@/components/navigation/nav-button';
 import LogoButton from '@/components/navigation/logo-button';
+import NavButton from '@/components/navigation/nav-button';
 import { useSelectedLayoutSegment } from 'next/navigation';
-import { BiBell, BiCog, BiHome, BiPencil, BiSolidBell, BiSolidCog, BiSolidHome, BiSolidPencil } from 'react-icons/bi';
-
-const NAV_ITEMS = [
-  { segment: null, defaultIcon: BiHome, activeIcon: BiSolidHome, to: '/' },
-  { segment: 'write', defaultIcon: BiPencil, activeIcon: BiSolidPencil, to: '/write' },
-  { segment: 'notification', defaultIcon: BiBell, activeIcon: BiSolidBell, to: '/notification' },
-  { segment: 'setting', defaultIcon: BiCog, activeIcon: BiSolidCog, to: '/setting' },
-] as const;
+import { LuChartLine, LuHouse, LuSearch, LuSquarePen, LuUser } from 'react-icons/lu';
 
 const AppNav = () => {
   const segment = useSelectedLayoutSegment();
   return (
-    <nav className="fixed bg-accent flex sm:bg-transparent sm:flex-col bottom-0 items-center sm:h-screen sm:top-0 shrink-0 sm:sticky w-full sm:w-14 sm:pb-20 z-50 sm:border-r">
+    <nav className="border-t sm:border-t-0 fixed h-16 flex bg-background sm:bg-transparent sm:flex-col bottom-0 items-center sm:h-screen sm:top-0 shrink-0 sm:sticky w-full sm:w-20 sm:pb-20 z-50">
       <LogoButton />
-      <div className="flex sm:flex-col justify-around sm:justify-center sm:items-center flex-1">
-        {NAV_ITEMS.map((item) => (
-          <NavButton
-            key={item.to}
-            to={item.to}
-            active={segment === item.segment}
-            defaultIcon={item.defaultIcon}
-            activeIcon={item.activeIcon}
-          />
-        ))}
+      <div className="flex sm:flex-col justify-around sm:justify-center sm:items-center flex-1 sm:gap-6">
+        <NavButton to="/" active={segment === null} Icon={LuHouse} />
+        <NavButton to="/search" active={segment === 'search'} Icon={LuSearch} />
+        <NavButton
+          to="/write"
+          active={segment === 'write'}
+          Icon={LuSquarePen}
+          className="bg-neutral-200 dark:bg-neutral-700"
+        />
+        <NavButton to="/stats" active={segment === 'stats'} Icon={LuChartLine} />
+        <NavButton to="/profile/user" active={segment === 'profile'} Icon={LuUser} />
       </div>
     </nav>
   );
