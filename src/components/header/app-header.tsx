d42933365edb1b3530acaf14c22bc972fbbe4490
sidamodev <sidamo.dev@gmail.com>
2026-02-23 18:14:00 +0900

    fix: 로고 hover 안되던 문제 수정

--- src/components/header/app-header.tsx
+++ src/components/header/app-header.tsx
@@ -1,14 +1,12 @@
 import LogoButton from '@/components/navigation/logo-button';
-import { LuSearch } from 'react-icons/lu';
 import ThemeSwitcher from '@/components/theme-switcher/theme-switcher';
 import { Kbd } from '@/components/ui/kbd';
+import { LuSearch } from 'react-icons/lu';
 
 const AppHeader = () => {
   return (
     <header className="sticky top-0 z-10 flex h-16 w-full items-center justify-between px-4 backdrop-blur-xl">
-      <div className="sm:ml-2">
-        <LogoButton className="w-12" />
-      </div>
+      <LogoButton className='sm:ml-2' />
 
       <div className="flex items-center gap-2">
         <button className="hidden sm:flex items-center gap-2 px-3 py-1.5 text-sm text-muted-foreground bg-muted/50 hover:bg-muted border rounded-md transition-colors w-64 justify-between">
